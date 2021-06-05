7441848b204f289cfccf2fcec3ec41fa4e4a154b
zamany <zamany1209@gmail.com>
2021-06-05 19:49:18 +0430

    zamany
    Edite Folders

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('product',[ProductController::class,'index']);
+Route::get('category',[CategoryController::class,'index']);
